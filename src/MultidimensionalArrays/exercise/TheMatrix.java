ee5ebd2ac3ebff8b78b7fad1e4328103dc5b695e
oziriuz <oziriuz@gmail.com>
2022-10-02 15:04:23 +0300

    matrix recursive dfs

--- src/MultidimensionalArrays/exercise/TheMatrix.java
+++ src/MultidimensionalArrays/exercise/TheMatrix.java
@@ -20,45 +20,36 @@ public class TheMatrix {
 
         if (isValidCoordinate(startR, startC, matrix)) {
             char initialSymbol = matrix[startR][startC];
-            matrix[startR][startC] = fillSymbol;
+            FillMatrix(fillSymbol, initialSymbol, startR, startC, matrix);
+        }
 
-            for (int row = 0; row < r; row++) {
-                for (int col = 0; col < c; col++) {
-                    if (matrix[row][col] == initialSymbol) {
-                        if (isFillSymbolNearMe(r, c, matrix, fillSymbol, row, col)) {
-                            matrix[row][col] = fillSymbol;
-                        }
-                    }
-                }
-                for (int col = c - 1; col >= 0; col--) {
-                    if (matrix[row][col] == initialSymbol) {
-                        if (isFillSymbolNearMe(r, c, matrix, fillSymbol, row, col)) {
-                            matrix[row][col] = fillSymbol;
-                        }
-                    }
-                }
-            }
+        printMatrix(matrix);
+    }
 
-            for (int col = 0; col < c; col++) {
-                for (int row = 0; row < r; row++) {
-                    if (matrix[row][col] == initialSymbol) {
-                        if (isFillSymbolNearMe(r, c, matrix, fillSymbol, row, col)) {
-                            matrix[row][col] = fillSymbol;
-                        }
-                    }
-                }
-                for (int row = r - 1; row >= 0; row--) {
-                    if (matrix[row][col] == initialSymbol) {
-                        if (isFillSymbolNearMe(r, c, matrix, fillSymbol, row, col)) {
-                            matrix[row][col] = fillSymbol;
-                        }
+    private static void FillMatrix(char fillSymbol, char initialSymbol, int startR, int startC, char[][] matrix) {
+        matrix[startR][startC] = fillSymbol;
+        if (!isValidCoordinate(startR, startC + 1, matrix) || matrix[startR][startC + 1] != initialSymbol) {
+            if (!isValidCoordinate(startR + 1, startC, matrix) || matrix[startR + 1][startC] != initialSymbol) {
+                if (!isValidCoordinate(startR, startC - 1, matrix) || matrix[startR][startC - 1] != initialSymbol) {
+                    if (!isValidCoordinate(startR - 1, startC, matrix) || matrix[startR - 1][startC] != initialSymbol) {
+                        return;
                     }
                 }
             }
         }
 
-        printMatrix(matrix);
-        //TODO: it is not complete solution, must use DFS or BFS
+        if (isValidCoordinate(startR, startC + 1, matrix) && matrix[startR][startC + 1] == initialSymbol) {
+            FillMatrix(fillSymbol, initialSymbol, startR, startC + 1, matrix);
+        }
+        if (isValidCoordinate(startR + 1, startC, matrix) && matrix[startR + 1][startC] == initialSymbol) {
+            FillMatrix(fillSymbol, initialSymbol, startR + 1, startC, matrix);
+        }
+        if (isValidCoordinate(startR, startC - 1, matrix) && matrix[startR][startC - 1] == initialSymbol) {
+            FillMatrix(fillSymbol, initialSymbol, startR, startC - 1, matrix);
+        }
+        if (isValidCoordinate(startR - 1, startC, matrix) && matrix[startR - 1][startC] == initialSymbol) {
+            FillMatrix(fillSymbol, initialSymbol, startR - 1, startC, matrix);
+        }
     }
 
     private static void printMatrix(char[][] matrix) {
@@ -68,13 +59,6 @@ public class TheMatrix {
             }
             System.out.println();
         }
-    }
-
-    private static boolean isFillSymbolNearMe(int r, int c, char[][] matrix, char fillSymbol, int row, int col) {
-        return (row < r - 1 && matrix[row + 1][col] == fillSymbol) ||
-                (col < c - 1 && matrix[row][col + 1] == fillSymbol) ||
-                (row > 0 && matrix[row - 1][col] == fillSymbol) ||
-                (col > 0 && matrix[row][col - 1] == fillSymbol);
     }
 
     private static char[][] getCharMatrixFromConsole(Scanner scanner, int r, int c) {
