fe17dd05e76af01ab717b84ff4e199ad05dd2520
oziriuz <oziriuz@gmail.com>
2022-10-08 18:31:35 +0300

    classes

---
+++ src/DefiningClasses/lab/Constructors/Main.java
@@ -0,0 +1,40 @@
+package DefiningClasses.lab.Constructors;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Set<Car> carSet = new LinkedHashSet<>();
+
+        while (n-- > 0) {
+            String[] input = scanner.nextLine().split("\\s+");
+
+            String brand = input[0];
+            Car car;
+            if (input.length == 1) {
+                car = new Car(brand);
+            } else {
+                String model = input[1];
+                if (input.length == 2) {
+                    car = new Car(brand, model);
+                } else {
+                    int hp = Integer.parseInt(input[2]);
+                    car = new Car(brand, model, hp);
+                }
+            }
+
+            carSet.add(car);
+        }
+
+        for (Car car : carSet) {
+            System.out.printf(car.carInfo());
+        }
+    }
+}
